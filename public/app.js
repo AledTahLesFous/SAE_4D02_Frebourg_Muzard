e9b67ffb98fbda34eb741f306b283888d7daedc4
AledTahLesFous <purpleponey2000@gmail.com>
2025-04-01 12:17:03 +0200

    save

--- public/app.js
+++ public/app.js
@@ -1,23 +1,12 @@
 // Création du SVG
 const width = 800, height = 600;
-const svg = d3.select("#graph")  // Assurez-vous que l'élément SVG est appendu à #graph
+const svg = d3.select("#graph")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
-    .style("cursor", "move");  // Pour indiquer que l'élément est déplaçable
 
-// Variable pour suivre la position de déplacement
-let offsetX = 0, offsetY = 0;
 
-// Ajouter le déplacement (drag) pour l'élément SVG
-svg.call(d3.drag()
-    .on("drag", function(event) {
-        offsetX = event.x;  // Mise à jour de l'offset X
-        offsetY = event.y;  // Mise à jour de l'offset Y
 
-        svg.attr("transform", `translate(${offsetX},${offsetY})`);
-    })
-);
 // Position initiale du premier point
 const center = { x: width / 2, y: height / 2 };
 
